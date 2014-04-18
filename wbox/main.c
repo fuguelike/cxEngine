5ee53a59664a4c03dab6af971ef0fff6557a2627
xuhua <xuhua@kongfz.com>
2014-04-18 16:25:03 +0800

    add wbox project

---
+++ wbox/main.c
@@ -0,0 +1,23 @@
+//
+//  cxMain.c
+//  cxEngine
+//
+//  Created by xuhua on 9/25/13.
+//  Copyright (c) 2013 xuhua. All rights reserved.
+//
+#include <core/cxEngine.h>
+
+void cxEngineInit(cxEngine engine)
+{
+    
+}
+
+void cxEngineMain(cxEngine engine)
+{
+    
+}
+
+void cxEngineFree(cxEngine engine)
+{
+    
+}
