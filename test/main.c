79be8e71fde2b742893a17f754b53f12eb3a553e
xuhua <xuhua@kongfz.com>
2014-06-04 16:17:42 +0800

    add test

--- test/main.c
+++ test/main.c
@@ -12,17 +12,15 @@ void cxEngineType(cxEngine engine)
     
 }
 
-#include <socket/cxHttp.h>
-
 void cxEngineInit(cxEngine engine)
 {
-    cxHttp http = cxHttpPost("http://127.0.0.1:9090", UTF8("1234"), true);
     cxEngineSetDesignSize(cxSize2fv(640, 960));
 }
 
 void cxEngineMain(cxEngine engine)
 {
-
+    cxViewLoader loader = cxViewLoaderCreate("cxScroll.json");
+    cxWindowPushView(loader->view);
 }
 
 void cxEngineFree(cxEngine engine)
