7b652c71ef9ed3fa6f1df277c9559c2c0a55366b
xuhua <xuhua@kongfz.com>
2014-08-26 17:43:58 +0800

    add

--- actions/cxParticle.c
+++ actions/cxParticle.c
@@ -287,9 +287,11 @@ CX_SETTER_DEF(cxParticle, texture)
     cxJson keys = cxJsonArray(value, "keys");
     cxInt keylen = cxJsonArrayLength(keys);
     CX_RETURN(keylen == 0);
+    //
     CX_RETAIN_SWAP(this->boxtexs, cxMemoryCreate(sizeof(cxBoxTex2f) * keylen));
     this->boxtexs->number = keylen;
     CX_METHOD_SET(this->GetBoxTex, cxParticleGetBoxTex);
+    //
     CX_JSON_ARRAY_EACH_BEG(keys, item)
     {
         cxBoxTex2f *boxs = this->boxtexs->data;
